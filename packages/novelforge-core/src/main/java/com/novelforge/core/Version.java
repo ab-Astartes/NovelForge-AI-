0ae6c32bc2cbf66b2455c0180c8c580b53a514f7
AB <1363139102@163.com>
2026-08-19 12:43:44 +0800

    chore: version bump to v0.6.0

--- packages/novelforge-core/src/main/java/com/novelforge/core/Version.java
+++ packages/novelforge-core/src/main/java/com/novelforge/core/Version.java
@@ -4,7 +4,7 @@ package com.novelforge.core;
  * NovelForge version constants. Updated on release.
  */
 public final class Version {
-    public static final String VERSION = "0.5.0";
+    public static final String VERSION = "0.6.0";
     public static final String NAME = "NovelForge";
 
     private Version() {} // prevent instantiation
